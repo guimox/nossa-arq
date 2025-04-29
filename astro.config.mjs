0ced45e94ef0ae6c54d67c1c6a91db908737ffe2
Guilherme Lopes <guilhermxlopes@gmail.com>
2025-04-29 07:02:56 -0300

    fix: react error config with vite/astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import cloudflare from '@astrojs/cloudflare';
-
 import react from '@astrojs/react';
 
 export default defineConfig({
+  output: "server",
   vite: {
     plugins: [tailwindcss()],
     resolve: {
