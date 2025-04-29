70d8d8c89348fddde264bba6a4f31816bbab05fa
Guilherme Lopes <guilhermxlopes@gmail.com>
2025-04-29 06:56:13 -0300

    fix: react error config with vite/astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,14 +7,12 @@ import react from '@astrojs/react';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
-  },
-  adapter: cloudflare(),
-  integrations: [react()],
-  vite: {
     resolve: {
       alias: import.meta.env.PROD && {
         "react-dom/server": "react-dom/server.edge",
       },
     },
   },
+  adapter: cloudflare(),
+  integrations: [react()],
 });
